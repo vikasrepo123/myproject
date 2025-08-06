3edabf22a3e32d4015e717b4c6c3fa3c5f772519
vikasrepo123 <vikasupadhyaythor@gmail.com>
2025-08-06 21:58:43 +0530

    Update calculation.sh

--- calculation.sh
+++ calculation.sh
@@ -2,3 +2,9 @@ read -p "Enter first number" n1
 read -p "Enter Second number" n2
 sum=$((n1 + n2))
 echo "Addition is $sum"
+#!/bin/bash
+while true
+do
+    echo "Script running at $(date)"
+    sleep 60
+done
